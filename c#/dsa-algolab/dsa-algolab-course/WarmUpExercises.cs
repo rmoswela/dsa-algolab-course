3e46a5ed6c02e6490cd60146453a5ac4ba5fd577
Reuben Moswela <Reuben.Moswela@derivco.co.za>
2023-01-10 10:42:04 +0200

    fizz buzz solution

---
+++ c#/dsa-algolab/dsa-algolab-course/WarmUpExercises.cs
@@ -0,0 +1,35 @@
+﻿namespace dsa_algolab_course
+{
+  public class WarmUpExercises
+  {
+    public string[] FizzBuzz(int n)
+    {
+      if (n < 1 || n > 104)
+      {
+        throw new ArgumentOutOfRangeException("n should be greater or equal to 1 and less or equal to 104!");
+      }
+
+      List<string> list = new List<string>();
+
+      for (int count = 0; count < n; count++) {
+        if (n % 3 == 0 && n % 5 == 0)
+        {
+          list.Add("FizzBuzz");
+        }
+        else if (n % 3 == 0)
+        {
+          list.Add("Fizz");
+        }
+        else if (n % 5 == 0)
+        {
+          list.Add("Buzz");
+        }
+        else
+        {
+          list.Add(count.ToString());
+        }
+      }
+      return list.ToArray();
+    }
+  }
+}

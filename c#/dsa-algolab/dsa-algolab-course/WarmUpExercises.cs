981daecc9401b11e48ad30a56bf65c0253962363
Reuben Moswela <Reuben.Moswela@derivco.co.za>
2023-01-10 14:50:08 +0200

    adds comments

--- c#/dsa-algolab/dsa-algolab-course/WarmUpExercises.cs
+++ c#/dsa-algolab/dsa-algolab-course/WarmUpExercises.cs
@@ -2,16 +2,27 @@
 {
   public class WarmUpExercises
   {
-    public string[] FizzBuzz(int n)
+    /// <summary>
+    /// Given an integer n, return a string array answer (1-indexed) where:
+    /// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    /// answer[i] == "Fizz" if i is divisible by 3.
+    /// answer[i] == "Buzz" if i is divisible by 5.
+    /// answer[i] == i (as a string) if none of the above conditions are true.
+    /// Constraints: 1 <= n <= 104
+    /// </summary>
+    /// <param name="number">This method takes an interger</param>
+    /// <returns>This method returns an array of strings</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public string[] FizzBuzz(int number)
     {
-      if (n < 1 || n > 104)
+      if (number < 1 || number > 104)
       {
         throw new ArgumentOutOfRangeException("n should be greater or equal to 1 and less or equal to 104!");
       }
 
       List<string> list = new List<string>();
 
-      for (int count = 1; count <= n; count++) {
+      for (int count = 1; count <= number; count++) {
         if (count % 3 == 0 && count % 5 == 0)
         {
           list.Add("FizzBuzz");
@@ -31,7 +42,11 @@
       }
       return list.ToArray();
     }
-
+    /// <summary>
+    /// Given an integer x, return true if x is a palindrome and false otherwise.
+    /// </summary>
+    /// <param name="number">This method takes in an interger</param>
+    /// <returns>This method returns a boolean</returns>
     public bool Palindrome (int number)
     {
       int reverse = 0;
